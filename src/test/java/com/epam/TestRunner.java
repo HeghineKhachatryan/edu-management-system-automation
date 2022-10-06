91d18d6cbbfbdd6f6309d2032db30be669c52255
davbalabekyan <balabekyandavo@gmai.com>
2022-10-06 12:39:12 +0400

    Add TestRunner class

--- src/test/java/com/epam/TestRunner.java
+++ src/test/java/com/epam/TestRunner.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\auth",glue = "com\\epam\\steps")
+@CucumberOptions(features = "src\\test\\resources\\features",
+        glue = "com\\epam\\steps",
+        publish = true)
 public class TestRunner {
 }
