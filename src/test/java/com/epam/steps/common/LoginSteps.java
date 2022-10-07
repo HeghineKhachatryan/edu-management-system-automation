95cee072f466920450bc03d6dd52a6cbca7e2ed7
Heghine Khachatryan <heghine_khachatryan@epam.com>
2022-10-07 12:27:37 +0400

    [feature/SCHOOL-137_integrate_logging_framework] added SLF4J logging framework

--- src/test/java/com/epam/steps/common/LoginSteps.java
+++ src/test/java/com/epam/steps/common/LoginSteps.java
@@ -14,6 +14,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,16 +43,19 @@ public class LoginSteps extends BaseSteps {
     @Given("Enter email {} in login page")
     public void fillEmailInLoginPage(String email) {
         loginPage.fillEmail(email);
+        logger.info("fill email: {}", email);
     }
 
     @And("Enter password {} in login page")
     public void fillPasswordInLoginPage(String password) {
         loginPage.fillPassword(password);
+        logger.info("fill password: {}", password);
     }
 
     @And("Click on 'login' button")
     public void clickOnLoginButton() {
         loginPage.clickOnLoginButton();
+        logger.info("Click on login button");
     }
 
     @Then("The user is on {} page")
